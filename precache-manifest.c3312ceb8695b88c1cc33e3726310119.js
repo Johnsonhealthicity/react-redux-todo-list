a88c30d525c317544b354ae2bbe071b9ddf24287
Johnson <johnson.koil@healthicity.com>
2024-06-23 22:30:54 +0530

    Updates

---
+++ precache-manifest.c3312ceb8695b88c1cc33e3726310119.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "495118c6490e93fede63",
+    "url": "/react-redux-todo-list/static/css/main.82141230.chunk.css"
+  },
+  {
+    "revision": "495118c6490e93fede63",
+    "url": "/react-redux-todo-list/static/js/main.d5f841c6.chunk.js"
+  },
+  {
+    "revision": "ed8ed661eec4a205dd16",
+    "url": "/react-redux-todo-list/static/js/runtime~main.3a998ffb.js"
+  },
+  {
+    "revision": "248e1b70f9636e2af398",
+    "url": "/react-redux-todo-list/static/js/2.20ac8a29.chunk.js"
+  },
+  {
+    "revision": "f73bbb14442ba952666d04a51b7eabb2",
+    "url": "/react-redux-todo-list/index.html"
+  }
+];
